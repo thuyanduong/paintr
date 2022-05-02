b24b6f5da40efd6ff0cd3ac40ff5256051e8b720
Ann Duong <thuyanannduong@gmail.com>
2022-05-02 09:50:08 -0400

    added about to home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar'
 import PaintingDetails from './PaintingDetails'
 import Gallery from './Gallery'
+import About from './About'
 import { useContext } from "react"
 import PainterContext from "../context/PainterContext"
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <div id="App" className="App">
       <Navbar />
+      <About />
       {currentPainting && <PaintingDetails />}
       <Gallery />
     </div>
